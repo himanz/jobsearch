81a99c3987a6065666c091f83cf8e2d26f682b42
Jonathan Chang <jonoschang7@gmail.com>
2014-09-25 16:59:19 -0400

    test for listing valid with attributes

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
 
 describe Listing do
-  it "is valid with a role, company and location"
+  it "is valid with a role, company and location" do
+  	listing = create(:listing)
+  	expect(listing).to be_valid
+  end
 
   it "is invalid without a role"
   it "is invalid without a company"
