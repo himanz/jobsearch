be375adb7d0beae5cc7150907667687a38529ae2
Jonathan Chang <jonoschang7@gmail.com>
2014-09-25 17:57:40 -0400

    listing not valid without a role

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,3 @@
 class Listing < ActiveRecord::Base
+	validates :role, presence: true
 end
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -6,7 +6,9 @@ describe Listing do
   	expect(listing).to be_valid
   end
 
-  it "is invalid without a role"
+  it "is invalid without a role" do
+  	expect(Listing.new(role: nil)).to have(1).errors_on(:role)
+  end
   it "is invalid without a company"
   it "is invalid without a location"
 end
