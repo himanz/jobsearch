1808375dc905909fe80f689de56086d649833033
Jonathan Chang <jonoschang7@gmail.com>
2014-09-26 16:35:07 -0400

    feature for adding new listing

--- spec/features/listings_spec.rb
+++ spec/features/listings_spec.rb
@@ -1,6 +1,21 @@
 require 'spec_helper'
 
 feature 'Listing management' do
+  scenario "add a new listing" do
+  	listing = create(:listing)
+  	visit new_listing_path
+  	expect {
+  		fill_in 'Role', with: listing.role
+  		fill_in 'Company', with: listing.company
+  		fill_in 'Location', with: listing.location
+  		click_button 'Submit'
+  	}.to change(Listing, :count).by(1)
+  	expect(current_path).to eq listing_path(listing.id + 1)
+  	expect(page).to have_content "Ruby Developer"
+		expect(page).to have_content "Ruby Workshop"
+		expect(page).to have_content "Toronto, ON"
+  end
+
 	scenario "click link of a listing in index" do
     create(:listing)
 
