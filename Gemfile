b2029623fe038b27278136d0e786b431d02991b2
Jonathan Chang <jonoschang7@gmail.com>
2014-09-27 17:23:57 -0400

    update rspec to recent version, fix deprecation warning

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
 group :development, :test do
-  gem "rspec-rails", '2.99'
+  gem "rspec-rails"
   gem "factory_girl_rails"
   gem "minitest"
   gem "capybara"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+# require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -40,5 +40,5 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
   config.include FactoryGirl::Syntax::Methods
-  config.infer_spec_type_from_file_location!
+  config.infer_spec_type_from_file_location!  
 end
