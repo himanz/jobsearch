b7b40ce382b905e991bef50d076bc3c8131276d6
Jonathan Chang <jonoschang7@gmail.com>
2014-09-28 14:44:09 -0400

    feature test to show correct headings

--- spec/features/listings_spec.rb
+++ spec/features/listings_spec.rb
@@ -18,11 +18,18 @@ feature 'Listing management' do
 
 	scenario "click link of a listing in index" do
     create(:listing)
-
 		visit listings_path
 		click_link 'Ruby Developer'
 		expect(page).to have_content "Ruby Developer"
 		expect(page).to have_content "Ruby Workshop"
 		expect(page).to have_content "Toronto, ON"
 	end
+
+  scenario "user views listings from today and yesterday" do
+    create(:listing)
+    create(:yesterday_listing)
+    visit listings_path
+    expect(page).to have_content "TODAY"
+    expect(page).to have_content "YESTERDAY"
+  end
 end
