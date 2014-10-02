c3ece18a61e49659b636c9fa8eba9a6010930353
Jonathan Chang <jonoschang7@gmail.com>
2014-10-02 16:02:18 -0400

    refactor toggle description

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,20 +1,18 @@
 $(document).ready(function() {
-	// Gets the relevant description of the listing from server
-  $('.show-description').on("click", function() {
-  	var button = $(this);
-  	var id = $(this).closest('div').attr("data-id");
-  	var listing = $(this).closest('.listing-row');
-
-  	$(button).remove();
-  	$(listing).append("<button class='toggle-description'>Description</button>");
-  	$.get('listings/' + id + '/description', function(data) {
-      $(listing).append("<div class='description'>"+ data + "</div>");  
-  	});
-  });
-
-  // Toggle show / hide of description
-  $('.toggle-description').on("click", function() {
-  	// $(this).closest('.listing-row').find(".description").toggle();
-  	$(this).next(".description").slideToggle();
+	// Gets the relevant description of the listing from server or toggles slide
+  $('.show-description').on("click", function(evt) {
+  	evt.stopImmediatePropagation();
+  	if ($(this).hasClass('toggle-description'))	 {
+  		$(this).closest('.listing-row').find(".description").slideToggle();
+  	} else {
+	  	var button = $(this);
+	  	var id = $(this).closest('div').attr("data-id");
+	  	var listing = $(this).closest('.listing-row');
+      $(button).addClass('toggle-description');
+	  	  
+	  	$.get('listings/' + id + '/description', function(data) {
+	      $(listing).append("<div class='description'>"+ data + "</div>");  
+	  	});
+    }
   });
 });
