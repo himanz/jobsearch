df4168a8bc7d10a01183467f0078501d4ccfdc4c
Jonathan Chang <jonoschang7@gmail.com>
2014-10-03 19:56:14 -0400

    create companies controller

---
+++ spec/controllers/companies_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+RSpec.describe CompaniesController, :type => :controller do
+  describe 'GET index' do
+  	it "populates an array with all the companies"
+  	it "renders the :index view"
+  end
+end
