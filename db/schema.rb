bcb504c4652a6813d1c9c4e50d8b3f1f3fe862eb
Jonathan Chang <jonoschang7@gmail.com>
2014-10-06 23:57:08 -0400

    add website to company

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007035359) do
+ActiveRecord::Schema.define(version: 20141007035630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20141007035359) do
     t.string   "location"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "website"
   end
 
   create_table "listings", force: true do |t|
